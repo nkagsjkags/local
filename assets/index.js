979d252a0818753056ef947aa9d13effe4bbbb06
nkagsjkags <prosy.papierosy@gmail.com>
2026-04-16 20:01:33 +0200

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,128 @@
+var selector = document.querySelector(".selector_box");
+selector.addEventListener("click", () => {
+  if (selector.classList.contains("selector_open")) {
+    selector.classList.remove("selector_open");
+  } else {
+    selector.classList.add("selector_open");
+  }
+});
+
+document.querySelectorAll(".date_input").forEach((element) => {
+  element.addEventListener("click", () => {
+    document.querySelector(".date").classList.remove("error_shown");
+  });
+});
+
+var sex = "m";
+
+document.querySelectorAll(".selector_option").forEach((option) => {
+  option.addEventListener("click", () => {
+    sex = option.id;
+    document.querySelector(".selected_text").innerHTML = option.innerHTML;
+  });
+});
+
+var upload = document.querySelector(".upload");
+
+var imageInput = document.createElement("input");
+imageInput.type = "file";
+imageInput.accept = ".jpeg,.png,.gif";
+
+document.querySelectorAll(".input_holder").forEach((element) => {
+  var input = element.querySelector(".input");
+  input.addEventListener("click", () => {
+    element.classList.remove("error_shown");
+  });
+});
+
+upload.addEventListener("click", () => {
+  imageInput.click();
+  upload.classList.remove("error_shown");
+});
+
+imageInput.addEventListener("change", (event) => {
+  upload.classList.remove("upload_loaded");
+  upload.classList.add("upload_loading");
+
+  upload.removeAttribute("selected");
+
+  var file = imageInput.files[0];
+  
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var dataUrl = e.target.result;
+    // Przechowaj w localStorage
+    localStorage.setItem("selectedImage", dataUrl);
+    
+    upload.classList.remove("error_shown");
+    upload.setAttribute("selected", "local");
+    upload.classList.add("upload_loaded");
+    upload.classList.remove("upload_loading");
+    upload.querySelector(".upload_uploaded").src = dataUrl;
+  };
+  reader.onerror = function() {
+    console.error("Błąd odczytywania zdjęcia");
+    upload.classList.remove("upload_loading");
+    upload.classList.add("error_shown");
+  };
+  reader.readAsDataURL(file);
+});
+
+document.querySelector(".go").addEventListener("click", () => {
+  var empty = [];
+
+  var params = new URLSearchParams();
+
+  params.set("sex", sex);
+  if (!upload.hasAttribute("selected")) {
+    empty.push(upload);
+    upload.classList.add("error_shown");
+  }
+
+  const day = document.getElementById("day");
+  const month = document.getElementById("month");
+  const year = document.getElementById("year");
+
+  [day, month, year].forEach((input) => {
+    if (isEmpty(input.value)) {
+      dateEmpty = true;
+    } else {
+      params.set(input.id, input.value);
+    }
+  });
+
+  document.querySelectorAll(".input_holder").forEach((element) => {
+    var input = element.querySelector(".input");
+
+    if (isEmpty(input.value)) {
+      empty.push(element);
+      element.classList.add("error_shown");
+    } else {
+      params.set(input.id, input.value);
+    }
+  });
+
+  if (empty.length != 0) {
+    empty[0].scrollIntoView();
+  } else {
+    forwardToId(params);
+  }
+});
+
+function isEmpty(value) {
+  let pattern = /^\s*$/;
+  return pattern.test(value);
+}
+
+function forwardToId(params) {
+  location.href = "./id.html?" + params;
+}
+
+var guide = document.querySelector(".guide_holder");
+guide.addEventListener("click", () => {
+  if (guide.classList.contains("unfolded")) {
+    guide.classList.remove("unfolded");
+  } else {
+    guide.classList.add("unfolded");
+  }
+});
